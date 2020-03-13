1ff3a57fca30ba11cb261bd55ab1d3be318888b0
wangrq <98709484@qq.com>
2020-03-14 00:23:39 +0800

    fix

--- founding02-admin-webui/src/test/java/com/local/crowd/CrowdTest.java
+++ founding02-admin-webui/src/test/java/com/local/crowd/CrowdTest.java
@@ -5,6 +5,8 @@ import com.local.crowd.entity.Admin;
 import com.local.crowd.mapper.AdminMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -24,10 +26,22 @@ public class CrowdTest {
     private AdminMapper adminMapper;
 
     @Test
+    public void testLog(){
+        //1. 获取Logger对象，这里传入的Class对象就是当前打印日志的类
+        Logger logger = LoggerFactory.getLogger(CrowdTest.class);
+
+        //2. 根据不同日志级别打印日志
+        logger.debug("hello debug");
+        logger.info("hello info");
+        logger.warn("hello warn");
+        logger.error("hello warn");
+    }
+
+    @Test
     public void testInsertAdmin() {
         Admin admin = new Admin(null, "nihao", "我好", "(((", "123@qq.com", null);
         int count = adminMapper.insert(admin);
-        System.out.println(count);
+//        System.out.println(count);
     }
 
     @Test
